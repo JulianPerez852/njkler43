deb6cc3a540147466176c83c0f25973f7bc53346
JulianPerez852 <jdph852@gmail.com>
2019-06-19 20:45:56 -0500

    commit para test

--- src/App.js
+++ src/App.js
@@ -5,17 +5,63 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      filteredData : [],
+    }
+    this.handlerChange=this.handlerChange.bind(this);
+  }
+
+  handlerChange(e){
+   const { value } = e.target;
+   const filteredData = 
+        posts.filter(p => 
+          this.normalizeFilterText(p.title.toLowerCase())
+                                    .includes(this.normalizeFilterText(value.toLowerCase())));
+    this.setState({ filteredData });
+  }
+
+  normalizeFilterText(filterText) {
+    const from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç",
+     to   = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
+     mapping = {};
+    
+    from
+     .split('')
+     .forEach((letter, index) => {
+        mapping[letter] = to.charAt(index);
+     });
+   
+    return this.convertFilterTextToNormalizeText(filterText, mapping);
+   
+  };
+  
+  convertFilterTextToNormalizeText(filterText, normalizeMapping) {
+    return filterText
+     .split('')
+     .map(letter => {
+       return normalizeMapping[letter] ? normalizeMapping[letter] : letter;
+     })
+     .join('');
+  }
+
+
   render() {
+    const { filteredData } = this.state;
+    const data = filteredData.length > 0 ? filteredData : posts;
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda"  onChange={this.handlerChange}/>
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
+          {data.map((post,index)=>
+          <li key={index}>
+            <a href={post.url}><img src={post.image } /></a>
+            <p>{ post.title }</p>
           </li>
+          )}
         </ul>
       </div>
     )
